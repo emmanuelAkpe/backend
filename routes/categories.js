d9e3040187a37f225ca22aa90482759f0d226ff5
E Akpe <eakpe2020\8@gmail.com>
2023-01-12 22:59:29 +0000

    categories routes updated

--- routes/categories.js
+++ routes/categories.js
@@ -12,49 +12,47 @@ router.get(`/`, async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const categoryId = req.params.id;
+  const category = await Category.findById(req.params.id);
 
-  const category = await Category.findById(categoryId);
   if (!category) {
-    res.status(404).json({ success: false, message: "category not found!" });
+    res
+      .status(500)
+      .json({ message: "The category with the given ID was not found." });
   }
   res.status(200).send(category);
 });
+
 router.post("/", async (req, res) => {
   let category = new Category({
     name: req.body.name,
     icon: req.body.icon,
     color: req.body.color,
   });
-
   category = await category.save();
-  if (!category) return res.status(404).send("the category was not created");
+
+  if (!category) return res.status(400).send("the category cannot be created!");
+
   res.send(category);
 });
 
 router.put("/:id", async (req, res) => {
-  const categoryId = req.params.id;
-  const category = Category.findByIdAndUpdate(
-    categoryId,
+  const category = await Category.findByIdAndUpdate(
+    req.params.id,
     {
       name: req.body.name,
-      icon: req.body.icon,
+      icon: req.body.icon || category.icon,
       color: req.body.color,
     },
-    {
-      new: true,
-    }
+    { new: true }
   );
 
-  if (!category) {
-    res.status(404).json({ success: false, message: "Category not found!" });
-  }
-  res.status(200).send(category);
+  if (!category) return res.status(400).send("the category cannot be created!");
+
+  res.send(category);
 });
 
-router.delete("/:id", async (req, res) => {
-  const Categoryid = req.params.id;
-  Category.findByIdAndRemove(Categoryid)
+router.delete("/:id", (req, res) => {
+  Category.findByIdAndRemove(req.params.id)
     .then((category) => {
       if (category) {
         return res
